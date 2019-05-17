f6ed76f9d6e5930626772d8072f703004d5373e4
Ved Kale <vedpkale@outlook.com>
2019-05-17 18:43:25 -0500

    Updates

---
+++ precache-manifest.d69194fc5cb530c32bb2ee86fa6a956d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf06096c59c551213c36dfa51fa8ba4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ca2fb1085a26756a89d",
+    "url": "/static/css/2.06565635.chunk.css"
+  },
+  {
+    "revision": "73dfa6a12834a1262e23",
+    "url": "/static/css/main.9dc41b9b.chunk.css"
+  },
+  {
+    "revision": "1ca2fb1085a26756a89d",
+    "url": "/static/js/2.4805192c.chunk.js"
+  },
+  {
+    "revision": "73dfa6a12834a1262e23",
+    "url": "/static/js/main.278ef3c2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "06cf07e34793681da9ed4f1e8a554db3",
+    "url": "/static/media/Resume.06cf07e3.pdf"
+  },
+  {
+    "revision": "e1ce60a0b2f0d9966169b5d5145eae36",
+    "url": "/static/media/WarmPoly.e1ce60a0.png"
+  },
+  {
+    "revision": "2c0862a67bfb8773d52c7833e17cd5ca",
+    "url": "/static/media/chevron.2c0862a6.svg"
+  },
+  {
+    "revision": "ba91083363d41d7177f0dafe5ad7dbfa",
+    "url": "/static/media/java.ba910833.png"
+  },
+  {
+    "revision": "e1a99557c8d9b1a506cb8f933d66100f",
+    "url": "/static/media/keras.e1a99557.svg"
+  },
+  {
+    "revision": "946732cf6f2013edeb81fe721d29fd02",
+    "url": "/static/media/python.946732cf.svg"
+  },
+  {
+    "revision": "e865e05f4497b97721dafafddaf88150",
+    "url": "/static/media/scrabble.e865e05f.png"
+  },
+  {
+    "revision": "0829d5e63032310ec565447ed1aa48d6",
+    "url": "/static/media/tennis_ball (1).0829d5e6.jpg"
+  },
+  {
+    "revision": "ed95dde251485ed4047c0e997bee8bc4",
+    "url": "/static/media/tensorflow.ed95dde2.svg"
+  }
+]);
